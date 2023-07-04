b293bba10e172e66f063f2bc394bef5ac5dc13d1
ohbamanato <al21005@shibaura-it.ac.jp>
2023-07-04 16:09:15 +0900

    Revert "bullet バグ修正"
    This reverts commit ad874f1c19ea803c98544ae965cc74ccf9bfe9d7.

--- MultipleTextEditor/Function/Form1.Bullet.cs
+++ MultipleTextEditor/Function/Form1.Bullet.cs
@@ -17,26 +17,8 @@ namespace MultipleTextEditor
     {
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
+            //箇条書き機能の有効化
             text_memo.SelectionBullet = true;
-            
-            
-            if (bullet.Checked)
-            {
-                text_memo.KeyPress += text_memo_KeyPress;
-            }
-            else
-            {
-                text_memo.KeyPress -= text_memo_KeyPress;
-            }
-        }
-
-        private void text_memo_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == (char)Keys.Enter)
-            {
-                text_memo.SelectionBullet = true;
-                e.Handled = true;
-            }
         }
     }
 }
