070c6cd20c612ca72ee2a8e0f3a52f66e55e653c
TaichiIwamoto <131168152+TaichiIwamoto@users.noreply.github.com>
2023-07-18 17:26:54 +0900

    Programヘッダ作成

--- MultipleTextEditor/Program.cs
+++ MultipleTextEditor/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*********************************************
+担当者:VSによる自動生成
+
+概要:アプリケーションのメイン エントリ ポイント
+*********************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
